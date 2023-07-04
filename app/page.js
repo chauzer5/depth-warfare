9d2d4802557fc55c8f013df08277d32c579218af
chauzer5 <chauzer.reed@gmail.com>
2023-07-04 07:20:18 -0600

    Added a count of people in the lobby

--- app/page.js
+++ app/page.js
@@ -1,32 +1,31 @@
 "use client";
 
-import { configureAbly, useChannel } from '@ably-labs/react-hooks'
+import { configureAbly, useChannel, usePresence } from '@ably-labs/react-hooks'
 import { useEffect, useState } from 'react';
 
 configureAbly({ key: "2KJZGA.aX_e0g:13USKhuP_xe_jQEIP1eUmkGsau-UUNCITFKa-ZqiU1A", clientId: Math.floor(Math.random() * 10000).toString()});
 
 export default function Home() {
   const [allMessages, setAllMessages] = useState([]);
+  const [numUsers, setNumUsers] = useState(0);
 
-  const [channel, ably] = useChannel("new-channel", (message) => {
-    addMessageToState(message);
+  const [channel] = useChannel("lobby", (message) => {
+    setAllMessages((prev) => [...prev, message]);
   });
 
-  const addMessageToState = (message) => {
-    console.log("PREXISTING MESSAGES");
-    console.log(allMessages);
-    setAllMessages((prev) => [...prev, message]);
-  };
-
+  const [presenceData, updateStatus] = usePresence("lobby");
+  
   useEffect(() => {
-    channel.publish("connected", {});
-  }, []);
+    setNumUsers(presenceData.length);
+  }, [presenceData]);
 
   return (
     <>
       <h1 onClick={() => {
+        console.log("ALL MESSAGES");
         console.log(allMessages);
       }}>MESSAGES</h1>
+      <p>Number of people in the lobby: {numUsers}</p>
       <ul>
         {allMessages.map((msg, index) => {
           return <li key={index}>{msg.name}</li>;
