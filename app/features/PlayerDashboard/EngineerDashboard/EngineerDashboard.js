c54fab575af784968604ad5c967bab70e870a3c0
Jessica Smith <jippsmith2@gmail.com>
2023-08-07 11:46:27 -0600

    engineer shrinking and growing

--- app/features/PlayerDashboard/EngineerDashboard/EngineerDashboard.js
+++ app/features/PlayerDashboard/EngineerDashboard/EngineerDashboard.js
@@ -25,6 +25,15 @@ export default function EngineerDashboard(){
             height: "30px",
             backgroundColor: "#0F0",       /*This will need to change later, so that it is a constant*/
             borderRadius: "15px",
+            // border: "4px solid #0F0",
+            marginRight: "10px",
+            transition: "width 1s ease",
+          },
+          rectangleBorder: {
+            flex: 1,
+            height: "30px",
+            backgroundColor: "#000",       /*This will need to change later, so that it is a constant*/
+            borderRadius: "18px",
             border: "4px solid #0F0",
             marginRight: "10px",
             transition: "width 1s ease",
@@ -61,6 +70,8 @@ export default function EngineerDashboard(){
     const [buttonStates, setButtonStates] = useState(
         new Array(NUM_BUTTONS).fill(true) // Assuming you have 5 buttons, initialize all to true
       );
+
+    const newButtonStates = [... buttonStates];
     
       // Function to handle button click
     const handleButtonClick = (index) => {
@@ -70,128 +81,219 @@ export default function EngineerDashboard(){
         // If there are already three active buttons and the current button is not active,
         // return early to prevent activating more buttons.
         if (activeCount === ACTIVE_BUTTONS && buttonStates[index]) {
-        return;
+          return;
         }
 
-        const newButtonStates = [...buttonStates]; // Create a copy of the array
+        // const newButtonStates = [...buttonStates]; // Create a copy of the array
         newButtonStates[index] = !newButtonStates[index]; // Toggle the state of the clicked button
         setButtonStates(newButtonStates); // Update the state with the new array
     };
     
 
     // Create a state variable to control the width of the blue rectangle
-    const [rectangleWidth, setRectangleWidth] = useState(100);
+    const [rectangleWidths, setRectangleWidths] = useState(
+      new Array(NUM_BUTTONS).fill(100)
+    );
 
-    // Function to start the shrinking animation
-    const startShrinking = () => {
-        // Update the rectangle width gradually to create the shrinking animation
-        let width = rectangleWidth;
-        const interval = setInterval(() => {
-          if (width <= 0.001) {
-            clearInterval(interval);
-            // Reset the rectangle width when the animation ends
-            setRectangleWidth(100);
-            console.log("Animation completed");
-          } else {
-            setRectangleWidth(width);
-            width -= 1; // Change the decrement value to control the speed of the animation
-          }
-        }, 10); // Adjust the interval duration to control the smoothness of the animation
-        console.log("startShrinking called");
-      };
+    //function for the shrinking rectangle
+    const startShrinking = (index) => {
+      // Update the rectangle width gradually to create the shrinking animation
+      const widths = [...rectangleWidths];
+      let width = widths[index];
     
-    // Function to reset the rectangle width when the component mounts or when the animation ends
-    // useEffect(() => {
-    //     setRectangleWidth(100);
-    //     console.log("useEffect triggered");
-    // }, [rectangleWidth]);
+      const interval = setInterval(() => {
+        // Shrink the rectangle by 10% each time the interval runs
+        width -= 10;
+    
+        // !!!!! THIS CODE WILL BE GOOD FOR THE CONTINUTOUS VERSION !!!!!!!!!
+        // if (width <= 0) {
+        //   clearInterval(interval);
+        //   console.log("Animation completed");            
+        // }
+
+        clearInterval(interval);
+        console.log("Animation completed");
+    
+        // Update the width in the state array
+        widths[index] = width;
+        setRectangleWidths([...widths]);
+      }, 1000); // Adjust the interval duration to control the smoothness of the animation
+      console.log("startShrinking called");
+    };
+
+    //function for the growing rectangle
+    const startGrowing = (index) => {
+      // Update the rectangle width gradually to create the shrinking animation
+      const widths = [...rectangleWidths];
+      let width = widths[index];
+
+      if (width == 100){
+        return;
+      }
+    
+      const interval = setInterval(() => {
+        // Shrink the rectangle by 10% each time the interval runs
+        width += 1;
+    
+        if (width >= 100) {
+          clearInterval(interval);
+          console.log("Animation completed");            
+        }
+
+        // Update the width in the state array
+        widths[index] = width;
+        setRectangleWidths([...widths]);
+      }, 1000); // Adjust the interval duration to control the smoothness of the animation
+      console.log("startGrowing called");
+    };
+
+
+    const handleActivateButtonClick = (index) => {
+      const currBool = buttonStates[index]
+
+      if(rectangleWidths[index] >= 10){
+        handleButtonClick(index);
+      }
+
+      if (buttonStates[index] == true && newButtonStates[index] == false){
+        startShrinking(index);
+      }
+
+  
+    };
+    
+    
     
     return (
         <>
 
     <div style={styles.container}>
-        <h5 style={styles.label}>Engineer</h5>
+        <h5 style={styles.label}>Sonar</h5>
 
-        <div
-          style={{
-            ...styles.rectangle,
-            width: `${rectangleWidth}%`,
-          }}
-        ></div>
-        <button style={styles.button} onClick={startShrinking}>
-          Click Me
-        </button>
+        <div style={styles.rectangleBorder}>
+          <div
+            style={{
+              ...styles.rectangle,
+              width: `${rectangleWidths[0]}%`,
+            }}
+          ></div>
+        </div>
 
         <button
           style={{
             ...styles.button,
             ...(buttonStates[0] ? styles.darkButton : {})
           }}
-          onClick={() => handleButtonClick(0)}
+          onClick={() => handleActivateButtonClick(0)}
         >
           Activate
         </button>
-        <button style={styles.button2}>Repair</button>
+        <button style={styles.button2} onClick={() => startGrowing(0)} >
+          Repair
+        </button>
       </div>
 
       <div style={styles.container}>
         <h5 style={styles.label}>Comms</h5>
-        <div style={styles.rectangle}></div>
+        <div style={styles.rectangleBorder}>
+          <div
+            style={{
+              ...styles.rectangle,
+              width: `${rectangleWidths[1]}%`,
+            }}
+          ></div>
+        </div>
+
+  
         <button
           style={{
             ...styles.button,
             ...(buttonStates[1] ? styles.darkButton : {})
           }}
-          onClick={() => handleButtonClick(1)}
+          onClick={() => handleActivateButtonClick(1)}
         >
           Activate
         </button>
-        <button style={styles.button2}>Repair</button>
+        <button style={styles.button2} onClick={() => startGrowing(1)} >
+          Repair
+        </button>
       </div>
 
       <div style={styles.container}>
-        <h5 style={styles.label}>Engine</h5>
-        <div style={styles.rectangle}></div>
+        <h5 style={styles.label}>Torpedo</h5>
+        <div style={styles.rectangleBorder}>
+          <div
+            style={{
+              ...styles.rectangle,
+              width: `${rectangleWidths[2]}%`,
+            }}
+          ></div>
+        </div>
+
         <button
           style={{
             ...styles.button,
             ...(buttonStates[2] ? styles.darkButton : {})
           }}
-          onClick={() => handleButtonClick(2)}
+          onClick={() => handleActivateButtonClick(2)}
         >
           Activate
         </button>
-        <button style={styles.button2}>Repair</button>
+        <button style={styles.button2} onClick={() => startGrowing(2)} >
+          Repair
+        </button>
       </div>
 
       <div style={styles.container}>
-        <h5 style={styles.label}>Life Support</h5>
-        <div style={styles.rectangle}></div>
+        <h5 style={styles.label}>Mine</h5>
+        <div style={styles.rectangleBorder}>
+          <div
+            style={{
+              ...styles.rectangle,
+              width: `${rectangleWidths[3]}%`,
+            }}
+          ></div>
+        </div>
+
+      
         <button
           style={{
             ...styles.button,
             ...(buttonStates[3] ? styles.darkButton : {})
           }}
-          onClick={() => handleButtonClick(3)}
+          onClick={() => handleActivateButtonClick(3)}
         >
           Activate
         </button>
-        <button style={styles.button2}>Repair</button>
+        <button style={styles.button2} onClick={() => startGrowing(3)} >
+          Repair
+        </button>
       </div>
 
       <div style={styles.container}>
-        <h5 style={styles.label}>Sonar</h5>
-        <div style={styles.rectangle}></div>
+        <h5 style={styles.label}>Engine</h5>
+        <div style={styles.rectangleBorder}>
+          <div
+            style={{
+              ...styles.rectangle,
+              width: `${rectangleWidths[4]}%`,
+            }}
+          ></div>
+        </div>
+
+
         <button
           style={{
             ...styles.button,
             ...(buttonStates[4] ? styles.darkButton : {})
           }}
-          onClick={() => handleButtonClick(4)}
+          onClick={() => handleActivateButtonClick(4)}
         >
           Activate
         </button>
-        <button style={styles.button2}>Repair</button>
+        <button style={styles.button2} onClick={() => startGrowing(4)} >
+          Repair
+        </button>
       </div>
     </>
   );
