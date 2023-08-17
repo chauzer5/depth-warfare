23161820174734a69646921c907086afd84e6820
Jessica Smith <jippsmith2@gmail.com>
2023-08-17 16:23:31 -0600

    Pop up screen for repair game

---
+++ app/features/PlayerDashboard/EngineerDashboard/RepairGame.js
@@ -0,0 +1,22 @@
+
+
+export default function RepairGame(props){
+    const {channel} = props;
+
+    const styles = {
+        text: {
+            fontSize: "25px",
+        }
+    }
+
+    // Functions for game goes here
+
+    return(
+        <div style = {styles.text}>
+            Lets fix this! 
+            <h5>
+                Testing to see if this actually works
+            </h5>
+        </div>
+    );
+};
