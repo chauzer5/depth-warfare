a39ca32e04d206d97d4b9cda53edc5a0913aca0e
moodyjam <jamisonmmoody@gmail.com>
2023-08-27 03:05:24 -0600

    Removed sort for repair matrix

--- app/state/game_state.js
+++ app/state/game_state.js
@@ -304,7 +304,7 @@ export function GameWrapper({children}) {
             doubledSystems.push("empty");
         }
 
-        const shuffledSystems = shuffleArray(doubledSystems).sort();
+        const shuffledSystems = shuffleArray(doubledSystems);
     
         let blankRepairMatrix = [];
     
