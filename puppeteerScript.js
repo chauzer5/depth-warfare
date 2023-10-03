3d7d905689dbfa7e9631ce13ed3e5253fd622013
Jessica Smith <jippsmith2@gmail.com>
2023-10-03 10:08:58 -0600

    puppeteer until capt pick point

--- puppeteerScript.js
+++ puppeteerScript.js
@@ -4,9 +4,8 @@ const puppeteer = require('puppeteer');
 const url = 'http://localhost:3000/';
 const numberOfTabs = 8;
 const userNameArray = ["player1", "player2", "player3", "player4", "player5", "player6" , "player7", "player8" ];
+const idArray = ["blueCaptain", "blueFirstMate", "blueEngineer", "blueRadioOperator", "redCaptain", "redFirstMate", "redEngineer", "redRadioOperator"];
 
-// const {useGameContext }= pkg
-// const { setUsername, setCurrentStage } = useGameContext();
 
 async function openTabs(){
     const browser = await puppeteer.launch({
@@ -20,6 +19,15 @@ async function openTabs(){
         await page.waitForSelector("input");
         await page.type("input", userNameArray[index]);
         await page.keyboard.press("Enter");
+        await page.waitForSelector(`#${idArray[index]}`);
+        const role = await page.$(`#${idArray[index]}`);
+        if (role) {
+            await role.click();
+        } else {
+            console.error(`${idArray[index]} button not found.`);
+        }
+        // await page.waitForSelector("blueCaptain");
+        // await page.click("blueCaptain");
         console.log(`Opened tab ${index + 1}`);
 
       });
