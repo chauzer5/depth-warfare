88a9f2177e72cb81c6620bd1b2601c4b9f31fe84
chauzer5 <chauzer.reed@gmail.com>
2023-11-20 09:18:39 -0700

    Added life support to all dashboard headers

--- app/features/PlayerDashboard/DashboardHeader.js
+++ app/features/PlayerDashboard/DashboardHeader.js
@@ -3,6 +3,8 @@ import dayjs from "dayjs";
 import { useState } from "react";
 import { ROLE_MAP } from "@/app/utils";
 import { useAblyContext } from "@/app/state/ably_state";
+import theme from "@/app/styles/theme";
+import SystemDamage from "./EngineerDashboard/SystemDamage";
 
 export default function DashboardHeader() {
   const { playerRole, playerTeam, hostClientId, roomCode } = useGameContext();
@@ -11,23 +13,54 @@ export default function DashboardHeader() {
   const styles = {
     main: {
       width: "100%",
-      height: "50px",
       display: "flex",
       flexDirection: "row",
       justifyContent: "space-between",
-      alignItems: "center",
+      alignItems: "flex-start",
+      // backgroundColor: "gray",
     },
     team: {
       color: playerTeam,
     },
     role: {
-      marginLeft: "15px",
+      margin: "0px",
       color: "white",
     },
     clock: {
-      marginRight: "15px",
+      margin: "0px",
       color: "white",
     },
+    roomCode: {
+      margin: "0px",
+      color: "white",
+    },
+    leftContainer: {
+      display: "flex",
+      margin: "10px",
+      flexDirection: "column",
+      // backgroundColor: "red",
+      alignItems: "flex-start",
+    },
+    rightContainer: {
+      display: "flex",
+      margin: "10px",
+      flexDirection: "column",
+      // backgroundColor: "purple",
+      alignItems: "flex-end",
+    },
+    lifeSupport: {
+      margin: "0px",
+      marginTop: "5px",
+      color: "gray",
+    },
+    dividersContainer: {
+      width: "365px",
+      height: "15px",
+      position: "absolute",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+    }
   };
 
   const [clock, setClock] = useState(dayjs().format("HH:mm:ss"));
@@ -40,17 +73,25 @@ export default function DashboardHeader() {
     .toLocaleTimeString("en-us", { timeZoneName: "short" })
     .split(" ")[2];
 
+  const { networkState } = useGameContext();
+  const { systemHealthLevels } = networkState;
+
   return (
-    <div style={styles.main}>
-      <h4 style={styles.role}>
-        <span style={styles.team}>{`${
-          playerTeam.charAt(0).toUpperCase() + playerTeam.slice(1)
-        } Team`}</span>
-        {` ${ROLE_MAP[playerRole]}`}
-      </h4>
-      {selfClientId === hostClientId && <h4 style={styles.role}>Host</h4>}
-      <h4 style={styles.role}>Room Code: {roomCode}</h4>
-      <h4 style={styles.clock}>{`${clock} ${timeZone}`}</h4>
-    </div>
+      <div style={styles.main}>
+        <div style={styles.leftContainer}>
+          <h4 style={styles.role}>
+            <span style={styles.team}>{`${
+              playerTeam.charAt(0).toUpperCase() + playerTeam.slice(1)
+            } Team`}</span>
+            {` ${ROLE_MAP[playerRole]}`}
+          </h4>
+          <SystemDamage system={{name: "life support", color: theme.gray}} />
+        </div>
+        {selfClientId === hostClientId && <h4 style={styles.role}>Host</h4>}
+        <div style={styles.rightContainer}>
+          <h4 style={styles.clock}>{`${clock} ${timeZone}`}</h4>
+          <h4 style={styles.roomCode}>Room Code: {roomCode}</h4>
+        </div>
+      </div>
   );
 }
